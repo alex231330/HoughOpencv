6c8479dafa664b53d32e64c99f176b5187422b4b
Alexandr <alexklatch@gmail.com>
2017-03-23 09:46:24 +0300

    Initial Commit

---
+++ HoughOpencv/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HoughOpencv
+//
+//  Created by Алексадр Тюльпанов on 23.03.17.
+//  Copyright © 2017 Алексадр Тюльпанов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
